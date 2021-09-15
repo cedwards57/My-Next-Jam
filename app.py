6e5c361174ec8f627f0c082534d4520c9855ddf4
Capella Edwards <32311207+cedwards57@users.noreply.github.com>
2021-09-15 22:29:11 +0000

    sptfy overhaul

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import flask
-import random
 from sptfy import get_info
 
 app = flask.Flask(__name__)
@@ -9,16 +8,16 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def index():
     sptfy_data = get_info()
-    releases = sptfy_data["releases"]
-    songs = sptfy_data["all_songs"]
+    random_song = sptfy_data["random_song"]
     return flask.render_template(
         "index.html",
-        releases = releases,
-        songs = songs,
-        random_song = random.choice(songs)
+        random_song = random_song
     )
 
-app.run(
-    host="0.0.0.0",
-    port=int(os.getenv("PORT", "8080"))
-)
+
+app.run()
+
+# app.run(
+#     host="0.0.0.0",
+#     port=int(os.getenv("PORT", "8080"))
+# )
--- sptfy.py
+++ sptfy.py
@@ -24,70 +24,71 @@ headers = {
 def get_info():
     """ Grabs all the Spotify info needed for the page. Imported to app.py. """
 
-    def get_releases():
-        """ Returns the latest 10 Spotify releases. """
-        params = {
-            "limit": 10
-        }
-        response = requests.get(BASE_URL + "browse/new-releases", headers=headers, params=params)
+    def get_track_info(track_id):
+        """ Return a dictionary of all necessary info for a single song. """
+
+        response = requests.get(BASE_URL + "tracks/" + track_id, headers=headers)
         response_json = response.json()
 
-        releases = response_json["albums"]["items"]
-        release_list = []
-        for i in releases:
-            release_list.append(i["name"])
-        return release_list
+        track_info = {
+            "name": response_json["name"],
+            "album": response_json["album"]["name"],
+            "album_id": response_json["album"]["id"],
+            "artist": response_json["album"]["artists"][0]["name"],
+            "artist_id": response_json["album"]["artists"][0]["id"],
+            "image_url": response_json["album"]["images"][0]["url"],
+            "preview_url": response_json["preview_url"]
+        }
+        return track_info
 
-    def get_albums(artist_id):
+    def get_random_track(album_id):
+        """ Returns a random song ID from the given album. """
+
+        response = requests.get(BASE_URL + "albums/" + album_id + "/tracks", headers=headers)
+        response_json = response.json()
+        songs = response_json["items"]
+
+        random_song = random.choice(songs)
+        random_song_id = random_song["id"]
+        return random_song_id
+
+    def get_random_album(artist_id):
+        """ Returns a random album or single ID from the given artist. """
         params = {
             "include_groups": "album,single"
         }
         response = requests.get(BASE_URL + "artists/" + artist_id + "/albums", headers=headers, params=params)
         response_json = response.json()
         albums = response_json["items"]
-        album_list = []
 
-        for i in albums:
-            album_list.append((i["id"],i["name"]))
-        return album_list
+        random_album = random.choice(albums)
+        random_album_id = random_album["id"]
+        return random_album_id
 
-    def get_all_songs(artist,album_list):
-        """ Returns a list of dicts of all the current artist's songs """
-        song_list = []
-        for i in album_list:
-            song_list.extend(get_album_songs(artist,i))
-        return song_list
-    
-    def get_album_songs(artist,album):
-        """ Returns a list of dicts about each song in the given album. """
-        album_response = requests.get(BASE_URL + "albums/" + album[0] + "/tracks", headers=headers)
-        album_response_json = album_response.json()
-        songs = album_response_json["items"]
-
-        album_songs = []
-        for j in songs:
-            this_song = {"name": j["name"], "id": j["id"], "artist": artist, "album": album[1]}
-            album_songs.append(this_song)
-
-        return album_songs
-
-    def get_artist_songs(artist,artist_id):
-        """ Returns a list of dictionaries with the name, artist, album, and ID of every song by the specified artist. Includes artist field because it gets compiled into a larger list with more artists later."""
-        return get_all_songs(artist,get_albums(artist_id))
-    
-    # Takes a dictionary of artists and returns a list of dictionaries identifying ALL the songs between them.
-    artist_list = {
+    my_artists = {
         "MARINA": "6CwfuxIqcltXDGjfZsMd9A",
         "The Hush Sound": "1RCoE2Dq19lePKhPzt9vM5",
         "The Family Crest": "44CB1c0W2h1XR2vB7AKpa7"
     }
-    all_song_list = []
-    for i in artist_list:
-        all_song_list.extend(get_artist_songs(i,artist_list[i]))
+    
+    random_song = get_track_info(get_random_track(get_random_album(random.choice(list(my_artists.values())))))
+
+    def get_artist_from_search(query):
+        params = {
+            "q": query,
+            "type": "artist",
+            "limit": 1
+        }
+
+        response = requests.get(BASE_URL + "search", headers=headers, params=params)
+        response_json = response.json()
+        artist_id = response_json["artists"]["items"][0]["id"]
+        return artist_id
+
+    query = "ABBA" # change to user input
+    random_song_from_input_artist = get_track_info(get_random_track(get_random_album(get_artist_from_search(query))))
 
     return {
-        "releases": get_releases(),
-        "all_songs": all_song_list
+        "random_song": random_song,
+        "random_song_output": random_song_from_input_artist
     }
-
-# change structure to artist: albums: songs
